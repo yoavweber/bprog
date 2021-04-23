e42faa2f456d653be612b9df6069a63ad4d4e0e5
yoavweber <yoav.weber@code.berlin>
2021-04-23 11:15:40 +0200

    adding length to listOp

--- src/Operations/ListOp.hs
+++ src/Operations/ListOp.hs
@@ -1,27 +1,31 @@
-module Operations.ListOp where
+module Operations.ListOp(handleListOp) where
     import Control.Monad.State
-    import Stack.StackOperations(pop, push)
+    import qualified Data.Map.Strict as M
+    
+    import Stack.StateOps(pop, push)
     import Parser
-
     import Types
 
 
     handleListOp :: String -> ProgState ()
     handleListOp t = case t of 
         "head" -> handleHead
+        -- "tail" -> handleTail 
+        "length" -> handleLength
+        -- "map"
         -- "length" -> handleLength
 
     handleHead :: ProgState ()
     handleHead = do
         list <- pop
-        -- push $ head' list
+        push $ head' list
         return ()
 
-    -- handleLength :: State Stack ()
-    -- handleLength = do
-    --     list <- pop
-    --     -- push $ length' list
-    --     return ()
+    handleLength :: ProgState ()
+    handleLength = do
+        list <- pop
+        push $ length' list
+        return ()
 
 
 
@@ -33,9 +37,9 @@ module Operations.ListOp where
     --     push $ head' list
     --     return ()
     -- TODO: check the type of the new element that you are pushing to the stack
-    -- head' :: StackElement  -> StackElement 
-    -- head' x = case x of
-    --    Literal (List l) -> Literal (assignLiteral (head l)) 
+    head' :: StackElement  -> StackElement 
+    head' x = case x of
+       Literal (List l) -> Literal ((head l)) 
         
         -- case x of
         -- List l -> StackString (head l)
@@ -43,12 +47,27 @@ module Operations.ListOp where
 
     -- tail' :: StackElement  -> StackElement 
     -- tail' x = case x of
-    --     List l -> Literal (assignLiteral $ head $ reverse l)
+    --     List l -> Literal (last l)
 
     
-    -- length' :: TokenType [a] -> StackElement
-    -- length' x = case x of
-    --     List l -> Literal (show $ length l)
+    length' :: StackElement -> StackElement
+    length' x = case x of
+        Literal ( List l) -> Literal $ StackInt (length l)
+
+
+
+
+
+    -- executeCodeLine :: String -> (AssignmentMap, Stack) -> Stack
+    -- executeCodeLine line (varMap,previousStack) = 
+    --     let stack =  (map (\e -> getTokenType e) $ tokenize (words line)) ++ previousStack
+    --     in snd (execState stackManip (varMap, stack))
+        
+
+    -- handleMap :: StackElement -> StackElement -> ProgState ()
+    -- map' list expr = 
+        
+
             
             
 
